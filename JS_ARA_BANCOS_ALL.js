13f6373de677a05c5b36af0cae39de74b776f1d1
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-02-18 14:54:28 -0300

    Update JS_ARA_BANCOS_ALL.js

--- JS_ARA_BANCOS_ALL.js
+++ JS_ARA_BANCOS_ALL.js
@@ -1,21 +1,22 @@
-$( document ).ready(function() {
-  if($('.marcas')[0]){
+        $(document).ready(function () {
 
-$('.Menu').append($('.marcas'));
+            if ($('.marcas')[0]) {
 
-} 
-else{
-var divMenu;
+                $('.Menu').append($('.marcas'));
 
-$.ajax({ url: 'https://sdasdsadsadasf.lojaintegrada.com.br/', success: function(data) { 
-  var html = data; 
-  var htmlElements = $(html);
-  divMenu = $('.marcas.hidden-phone', htmlElements);
-  $('.Menu').append(divMenu);
-} });
+            }
+            else {
+                var divMenu;
 
+                $.ajax({
+                    url: 'https://sdasdsadsadasf.lojaintegrada.com.br/', success: function (data) {
+                        var html = data;
+                        var htmlElements = $(html);
+                        divMenu = $('.marcas.hidden-phone', htmlElements);
+                        $('.Menu').append(divMenu);
+                    }
+                });
 
-
-
-}  
-   });
+            }
+            $('.flex-direction-nav').remove()
+        });
