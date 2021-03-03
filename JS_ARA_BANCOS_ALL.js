92f1be5703879a39b864436e04d70beaf4c20c7a
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-03-03 11:22:36 -0300

    Update JS_ARA_BANCOS_ALL.js

--- JS_ARA_BANCOS_ALL.js
+++ JS_ARA_BANCOS_ALL.js
@@ -1,42 +1,26 @@
         $(document).ready(function () {
-            function Mudar_Posicao() {
-                $('#fb-root').after($('.Container_Menu_Cima')[0]);
-                $('.Container_Menu_Cima').after($('.Sub_Menu_Marcas')[0]);
-            }
-            function Carregar_Menu() {
-                if ($('.marcas')[0]) {
 
-                    $('.Menu').append($('.marcas'));
+            $('#fb-root').after($('.Container_Menu_Cima')[0]);
+            $('.Container_Menu_Cima').after($('.Sub_Menu_Marcas')[0]);
 
-                }
-                else {
-                    var divMenu;
+            if ($('.marcas')[0]) {
 
-                    $.ajax({
-                        url: 'https://sdasdsadsadasf.lojaintegrada.com.br/', success: function (data) {
-                            var html = data;
-                            var htmlElements = $(html);
-                            divMenu = $('.marcas.hidden-phone', htmlElements);
-                            $('.Menu').append(divMenu);
-                        }
-                    });
-                }
+                $('.Menu').append($('.marcas'));
+
             }
-            function Remover_Elementos() {
-                $('.flex-direction-nav')[0].remove();
-            }
-            
-            function Gerar_ID_Menu() {
-                var lis = $('.flex-viewport .slides li');
-                lis.each(function (li) {
-                    $(this).prop('id', $(this).text().replaceAll(" ", "").replaceAll("\n", ""));
+            else {
+                var divMenu;
+
+                $.ajax({
+                    url: 'https://sdasdsadsadasf.lojaintegrada.com.br/', success: function (data) {
+                        var html = data;
+                        var htmlElements = $(html);
+                        divMenu = $('.marcas.hidden-phone', htmlElements);
+                        $('.Menu').append(divMenu);
+                    }
                 });
             }
-            Mudar_Posicao();
-            Carregar_Menu();
-            Remover_Elementos();
-            Gerar_ID_Menu();
 
 
-
+             $('.flex-direction-nav')[0].remove()
         });
