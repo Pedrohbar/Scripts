3b2cb25adb4c37d3c3f34568db2e78ca8439e7f1
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-03-03 10:14:47 -0300

    Update JS_ARA_BANCOS_HOME.js

--- JS_ARA_BANCOS_HOME.js
+++ JS_ARA_BANCOS_HOME.js
@@ -1,9 +1,4 @@
 $( document ).ready(function() {
    $('.secao-banners').after($('.Container_Selecionar_Motocicleta')[0]);
-   
-   var lis = $('.marcas > div > div > .slides > li');
-lis.each(function(li) {
-  $(this).prop('id', $(this).text().replaceAll(" ", "").replaceAll("\n", ""));
-});
-   
+     
    });
