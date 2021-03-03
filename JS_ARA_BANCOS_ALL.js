2fd1ae89e9f20818c791979886144cbe558da434
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-03-03 10:45:51 -0300

    Create JS_ARA_BANCOS_ALL.js

--- JS_ARA_BANCOS_ALL.js
+++ JS_ARA_BANCOS_ALL.js
@@ -23,7 +23,14 @@
                     }
                 });
             }
-
+            function testamento(){
+                var lis = $('.flexslider .slides:eq(0) li');
+                lis.each(function(li) {
+                        $(this).prop('id', $(this).text().replaceAll(" ", "").replaceAll("\n", ""));
+});
+}
+                testamento()
              $('.flex-direction-nav')[0].remove()
         });
+
             
