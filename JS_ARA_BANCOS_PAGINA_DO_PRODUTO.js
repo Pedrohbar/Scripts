e22388d00d24e30e2572bacd6bb9beacfd8df4da
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-03-03 11:55:11 -0300

    Update JS_ARA_BANCOS_PAGINA_DO_PRODUTO.js

--- JS_ARA_BANCOS_PAGINA_DO_PRODUTO.js
+++ JS_ARA_BANCOS_PAGINA_DO_PRODUTO.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
     $(".carrinho i,.carrinho span, .cep, #DelimiterFloat").remove();
     $( ".span6:eq(1)" ).addClass("Edit_Img");
-    $(".produto .row-fluid:eq(0) .span6:eq(1)").addClass("Edit_Comprar")
-    $(".conteiner:eq(2)").addClass("Edit_Container_Produto")
-    $(".codigo-produto .cor-secundaria:eq(0) ").addClass("Codigo_Produto")
-    $(".Icone_Carrinho a .Icone_Carrinho_Edit").after($(".carrinho:eq(0)"))
-    $(".conteiner-imagem").before($(".breadcrumbs"))
+    $(".produto .row-fluid:eq(0) .span6:eq(1)").addClass("Edit_Comprar");
+    $(".conteiner:eq(2)").addClass("Edit_Container_Produto");
+    $(".codigo-produto .cor-secundaria:eq(0) ").addClass("Codigo_Produto");
+    $(".Icone_Carrinho a .Icone_Carrinho_Edit").after($(".carrinho:eq(0)"));
+    $(".conteiner-imagem").before($(".breadcrumbs"));
     var lis = $('.flexslider:eq(0) .slides li');
     lis.each(function (li) {
         $(this).prop('id', $(this).text().replaceAll(" ", "").replaceAll("\n", ""));
