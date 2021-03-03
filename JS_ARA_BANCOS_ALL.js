d11e9195b7cb636d0464b80dfc5c9c5b84d9f311
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-03-03 11:15:20 -0300

    Update JS_ARA_BANCOS_ALL.js

--- JS_ARA_BANCOS_ALL.js
+++ JS_ARA_BANCOS_ALL.js
@@ -1,26 +1,42 @@
-
         $(document).ready(function () {
-
-            $('#fb-root').after($('.Container_Menu_Cima')[0]);
-            $('.Container_Menu_Cima').after($('.Sub_Menu_Marcas')[0]);
-
-            if ($('.marcas')[0]) {
-
-                $('.Menu').append($('.marcas'));
-
+            function Mudar_Posicao() {
+                $('#fb-root').after($('.Container_Menu_Cima')[0]);
+                $('.Container_Menu_Cima').after($('.Sub_Menu_Marcas')[0]);
             }
-            else {
-                var divMenu;
+            function Carregar_Menu() {
+                if ($('.marcas')[0]) {
 
-                $.ajax({
-                    url: 'https://sdasdsadsadasf.lojaintegrada.com.br/', success: function (data) {
-                        var html = data;
-                        var htmlElements = $(html);
-                        divMenu = $('.marcas.hidden-phone', htmlElements);
-                        $('.Menu').append(divMenu);
-                    }
+                    $('.Menu').append($('.marcas'));
+
+                }
+                else {
+                    var divMenu;
+
+                    $.ajax({
+                        url: 'https://sdasdsadsadasf.lojaintegrada.com.br/', success: function (data) {
+                            var html = data;
+                            var htmlElements = $(html);
+                            divMenu = $('.marcas.hidden-phone', htmlElements);
+                            $('.Menu').append(divMenu);
+                        }
+                    });
+                }
+            }
+            function Remover_Elementos() {
+                $('.flex-direction-nav')[0].remove();
+            }
+            
+            function Gerar_ID_Menu() {
+                var lis = $('.flex-viewport .slides li');
+                lis.each(function (li) {
+                    $(this).prop('id', $(this).text().replaceAll(" ", "").replaceAll("\n", ""));
                 });
             }
+            Mudar_Posicao();
+            Carregar_Menu();
+            Remover_Elementos();
+            Gerar_ID_Menu();
 
-             $('.flex-direction-nav')[0].remove()
+
+
         });
