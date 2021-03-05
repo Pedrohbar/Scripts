7ea033b1d76a05b687a4cecd96119f379204de60
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-03-05 18:47:07 -0300

    Update JS_ARA_BANCOS_ALL.js

--- JS_ARA_BANCOS_ALL.js
+++ JS_ARA_BANCOS_ALL.js
@@ -1,10 +1,39 @@
         $(document).ready(function () {
                 function Gerar_ID(){
-         var lis = $('.flexslider:eq(0) ul li');
-                lis.each(function (li) {
-                    $(this).prop('id', $(this).text().trim().replace(" ","-").replaceAll("\n", "").toLowerCase());
-                     
-});
+   
+var lis = $('.flexslider:eq(0) ul li');
+
+    lis.each(function (liuu) {
+        var a = $(this).prop('id', $(this).text().trim().replace(" ", "-").replaceAll("\n", "").toLowerCase());
+
+        a.mouseenter(function () {
+            $(".Sub_Menu_Marcas").css("display", "block");
+
+            var b = $.ajax({
+                url: "https://sdasdsadsadasf.lojaintegrada.com.br/marca/" + a.text().trim().replace(" ", "-").toLowerCase() + ".html", success: function (atoo) {
+                    var htmldois = atoo;
+                    var htmlElementsDois = $(htmldois);
+                    b = $("#listagemProdutos", htmlElementsDois);
+                    var Remover_Excesso = $(".Sub_Menu_Marcas").children().length;
+                    $(".Sub_Menu_Marcas").append(b);
+
+                    if (Remover_Excesso >= 1) {
+                        $(".Sub_Menu_Marcas").children().remove();
+                        $(".Sub_Menu_Marcas").append(b);
+                    }
+
+                }
+
+            })
+        });
+
+
+        $(".flexslider:eq(0)").mouseleave(function () {
+            $(".Sub_Menu_Marcas").css("display", "none")
+
+        });
+
+    });
                 }
                          
 
@@ -24,11 +53,13 @@
                         var htmlElements = $(html);
                         Puxar_Menu_Marcas = $('.marcas', htmlElements);
                         $('.Menu').append(Puxar_Menu_Marcas);
+                        $(".flex-viewport").after($(".Sub_Menu_Marcas")); 
                         Gerar_ID();                        
                     }
                 });
 
                 } 
-        Gerar_ID();
+        $(".flex-viewport").after($(".Sub_Menu_Marcas")); 
+         Gerar_ID();
          $('.flex-direction-nav')[0].remove()
         });
