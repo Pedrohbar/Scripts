89b58d7a245f5f4682be0bd51433c2d93b7a66b4
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-03-06 11:13:28 -0300

    Update JS_ARA_BANCOS_ALL.js

--- JS_ARA_BANCOS_ALL.js
+++ JS_ARA_BANCOS_ALL.js
@@ -1,70 +1,59 @@
-        $(document).ready(function () {
-                function Gerar_ID(){
-   
-lis.each(function (liuu) {
-   var a = $(this).prop('id', $(this).text().trim().replace(" ", "-").replaceAll("\n", "").toLowerCase());
-   var liId = $(this).prop('id');
+$(document).ready(function () {
+    function Gerar_ID() {
+        lis.each(function (liuu) {
+            var a = $(this).prop('id', $(this).text().trim().replace(" ", "-").replaceAll("\n", "").toLowerCase());
+            var liId = $(this).prop('id');
 
-	var b = $.ajax({
-		url: "https://sdasdsadsadasf.lojaintegrada.com.br/marca/" + a.text().trim().replace(" ", "-").toLowerCase() + ".html", success: function (atoo) {
-			var htmldois = atoo;
-			var htmlElementsDois = $(htmldois);
-			b = $("#listagemProdutos", htmlElementsDois);
-			
-			b.prop('id', 'listagemProdutos-' + liId);
-			b.css('display', 'none');
-			
-			$(".Sub_Menu_Marcas").append(b);
-		}
-	});
-		
-	a.mouseenter(function () {
-		$(".Sub_Menu_Marcas").css("display", "block");
-		$(".Sub_Menu_Marcas > .listagem.borda-alpha").css("display", "none");
-		$('#listagemProdutos-' + liId).css('display', 'block');
-	});
+            var b = $.ajax({
+                url: "https://sdasdsadsadasf.lojaintegrada.com.br/marca/" + a.text().trim().replace(" ", "-").toLowerCase() + ".html", success: function (atoo) {
+                    var htmldois = atoo;
+                    var htmlElementsDois = $(htmldois);
+                    b = $("#listagemProdutos", htmlElementsDois);
 
-	$(".flexslider:eq(0)").mouseleave(function () {
-		$(".Sub_Menu_Marcas").css("display", "none")
+                    b.prop('id', 'listagemProdutos-' + liId);
+                    b.css('display', 'none');
 
-	});
-
-});
-    
-
-
-        $(".flexslider:eq(0)").mouseleave(function () {
-            $(".Sub_Menu_Marcas").css("display", "none")
-
-        });
-
-    });
+                    $(".Sub_Menu_Marcas").append(b);
                 }
-                         
+            });
 
-            $('#fb-root').after($('.Container_Menu_Cima')[0]);
-            $('.Container_Menu_Cima').after($('.Sub_Menu_Marcas')[0]);
+            a.mouseenter(function () {
+                $(".Sub_Menu_Marcas").css("display", "block");
+                $(".Sub_Menu_Marcas > .listagem.borda-alpha").css("display", "none");
+                $('#listagemProdutos-' + liId).css('display', 'block');
+            });
 
-            if ($('.marcas')[0]) {
+            $(".flexslider:eq(0)").mouseleave(function () {
+                $(".Sub_Menu_Marcas").css("display", "none")
 
-                $('.Menu').append($('.marcas'));
+            });
 
-            }
-            else {
-                  //requisitar ajax para puxar os elementos das outras paginas  
-                var Puxar_Menu_Marcas = $.ajax({
-                    url: 'https://sdasdsadsadasf.lojaintegrada.com.br', success: function (aata) {
-                        var html = aata;
-                        var htmlElements = $(html);
-                        Puxar_Menu_Marcas = $('.marcas', htmlElements);
-                        $('.Menu').append(Puxar_Menu_Marcas);
-                        $(".flex-viewport").after($(".Sub_Menu_Marcas")); 
-                        Gerar_ID();                        
-                    }
-                });
-
-                } 
-        $(".slides:eq(0)").after($(".Sub_Menu_Marcas")); 
-         Gerar_ID();
-         $('.flex-direction-nav')[0].remove()
         });
+
+    }
+
+
+    $('#fb-root').after($('.Container_Menu_Cima')[0]);
+    $('.Container_Menu_Cima').after($('.Sub_Menu_Marcas')[0]);
+
+    if ($('.marcas')[0]) {
+
+        $('.Menu').append($('.marcas'));
+
+    }
+    else {
+        //requisitar ajax para puxar os elementos das outras paginas  
+        var Puxar_Menu_Marcas = $.ajax({
+            url: 'https://sdasdsadsadasf.lojaintegrada.com.br', success: function (aata) {
+                var html = aata;
+                var htmlElements = $(html);
+                Puxar_Menu_Marcas = $('.marcas', htmlElements);
+                $('.Menu').append(Puxar_Menu_Marcas);
+                Gerar_ID();
+            }
+        });
+
+    }
+    Gerar_ID();
+    $('.flex-direction-nav')[0].remove()
+});
