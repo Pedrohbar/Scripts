50112643b3043de9541b567b79bfac3b8bb4ebf3
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-03-10 12:51:09 -0300

    Update JS_ARA_BANCOS_PAGINA_DO_PRODUTO.js

--- JS_ARA_BANCOS_PAGINA_DO_PRODUTO.js
+++ JS_ARA_BANCOS_PAGINA_DO_PRODUTO.js
@@ -7,5 +7,4 @@ $( document ).ready(function() {
     $(".Icone_Carrinho a .Icone_Carrinho_Edit").after($(".carrinho:eq(0)"));
     $(".conteiner-imagem").before($(".breadcrumbs"));
     $(".Edit_Comprar .principal").not(".botao-comprar").addClass("Editar_div_Comprar");
-    $(".acoes-produto.hide.disponivel").children().not(".comprar").addClass("Editar_Preco_Sem_Classe")
 });
