05d5da3d5bb2dd776f3eee6687900e21b74e092c
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-03-12 16:02:45 -0300

    Update JS_ARA_BANCOS_ALL.js

--- JS_ARA_BANCOS_ALL.js
+++ JS_ARA_BANCOS_ALL.js
@@ -95,7 +95,7 @@ $(document).ready(function () {
         });
 
         $('head').append(Link);
-        $('#id_tipo_0:eq(0)').append(Div_Botao);
+        $('#id_tipo li label:eq(0)').append(Div_Botao);
         $("#corpo").after($(".Footer"))
 
 
