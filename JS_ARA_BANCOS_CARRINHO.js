d926fbfd9b578efdac3f247190ed490c0e966188
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-03-15 15:57:08 -0300

    Update JS_ARA_BANCOS_CARRINHO.js

--- JS_ARA_BANCOS_CARRINHO.js
+++ JS_ARA_BANCOS_CARRINHO.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
   $(".produto-info ul li:nth-child(2)").addClass("Li_Estoque");
-  $("tbody .hidden-phone").not(".bg-dark").addClass("Preco")
+  $("tbody .hidden-phone").not(".bg-dark").addClass("Preco");
 });
 
