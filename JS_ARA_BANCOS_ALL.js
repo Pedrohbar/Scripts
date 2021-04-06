c4f2e93755e462105a30fa55b95c6bb8fbed5c7d
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-04-06 12:38:14 -0300

    Update JS_ARA_BANCOS_ALL.js

--- JS_ARA_BANCOS_ALL.js
+++ JS_ARA_BANCOS_ALL.js
@@ -101,6 +101,6 @@ $(document).ready(function () {
 
 
     }
-    $('.flex-direction-nav:eq(0), #rodape, .modal-backdrop, #AcessoRestritoIdade').remove()
+    $('.flex-direction-nav:eq(0), .modal-backdrop, #AcessoRestritoIdade').remove()
 
 });
