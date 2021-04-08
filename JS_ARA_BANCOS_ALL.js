6ae8e5fae1ee9dc7890f9043df06e11d3e49bf17
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-04-08 14:09:42 -0300

    Update JS_ARA_BANCOS_ALL.js

--- JS_ARA_BANCOS_ALL.js
+++ JS_ARA_BANCOS_ALL.js
@@ -1,30 +1,14 @@
 $(document).ready(function () {
+
     function Gerar_ID() {
 
         var lis = $('.flexslider.carousel:eq(0) ul li');
 
         lis.each(function (liuu) {
-            var a = $(this).prop('id', $(this).text().trim().replace(" ", "-").replaceAll("\n", "").toLowerCase());
-            var liId = $(this).prop('id');
 
-            var b = $.ajax({
-                url: "https://sdasdsadsadasf.lojaintegrada.com.br/marca/" + a.text().trim().replace(" ", "-").toLowerCase() + ".html", success: function (atoo) {
-                    var htmldois = atoo;
-                    var htmlElementsDois = $(htmldois);
-                    b = $("#listagemProdutos", htmlElementsDois);
+            var a = $(this).prop("id", $(this).text().trim().replace(" ", "-").replaceAll("\n", "").toLowerCase());
 
-
-                    b.prop('id', 'listagemProdutos-' + liId);
-                    b.css('display', 'none');
-                    b.addClass("Edit_Listas_Sub_Menu_Marcas");
-                    b.children().find(".preco-produto, .acoes-produto").remove();
-                    b.children().find(".span3").addClass("Editar_li_Sub_Marcas");
-                    b.children().find(".info-produto").addClass("Editar_Texto_Sub_Marcas");
-                    b.children().find(".listagem-item").addClass("Editar_Marcas_Moto");
-                    b.children().find(".imagem-produto").addClass("Editar_Img_Sub_Marcas");
-                    $(".Sub_Menu_Marcas").append(b);
-                }
-            });
+            var liId = $(this).prop("id");
 
             a.mouseenter(function () {
                 $(".Sub_Menu_Marcas").css("display", "block");
@@ -51,25 +35,11 @@ $(document).ready(function () {
     $('#fb-root').after($('.Container_Menu_Cima')[0]);
     $('.Container_Menu_Cima').after($('.Sub_Menu_Marcas')[0]);
 
-    if ($('.marcas')[0]) {
-
-        $('.Menu').append($('.marcas'));
-
-    }
-    else {
-        //requisitar ajax para puxar os elementos das outras paginas  
-        var Puxar_Menu_Marcas = $.ajax({
-            url: 'https://sdasdsadsadasf.lojaintegrada.com.br', success: function (aata) {
-                var html = aata;
-                var htmlElements = $(html);
-                Puxar_Menu_Marcas = $('.marcas', htmlElements);
-                $('.Menu').append(Puxar_Menu_Marcas);
-                Gerar_ID();
-            }
-        });
-
-    }
+    
     Gerar_ID();
+
+
+    //Editar dinamicamente paginas sem acesso
     if (window.location.pathname.includes("/conta/login")) {
 
         var link = $("<link />", {
@@ -101,6 +71,7 @@ $(document).ready(function () {
 
 
     }
+
     $("#rodape").children().not(".institucional, .pagamento-selos").remove();
     $('.flex-direction-nav:eq(0), .modal-backdrop, #AcessoRestritoIdade, .institucional .conteiner .row-fluid .span9 .row-fluid .span4 .titulo, .pagamento-selos').remove();
 
