f08cab32e841b097b092d91e004694c72f671182
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-04-12 11:26:55 -0300

    Update JS_ARA_BANCOS_ALL.js

--- JS_ARA_BANCOS_ALL.js
+++ JS_ARA_BANCOS_ALL.js
@@ -31,7 +31,8 @@ $(document).ready(function () {
 
     }
 
-
+    $("#rodape").children().not(".institucional, .pagamento-selos").remove();
+    $('.flex-direction-nav:eq(0), .modal-backdrop, #AcessoRestritoIdade, .institucional .conteiner .row-fluid .span9 .row-fluid .span4 .titulo, .pagamento-selos').remove();
     $('#fb-root').after($('.Container_Menu_Cima')[0]);
     $('.Container_Menu_Cima').after($('.Sub_Menu_Marcas')[0]);
 
@@ -72,8 +73,5 @@ $(document).ready(function () {
 
 
     }
-
-    $("#rodape").children().not(".institucional, .pagamento-selos").remove();
-    $('.flex-direction-nav:eq(0), .modal-backdrop, #AcessoRestritoIdade, .institucional .conteiner .row-fluid .span9 .row-fluid .span4 .titulo, .pagamento-selos').remove();
 
 });
