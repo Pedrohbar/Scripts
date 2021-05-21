c20095cb77c92284c1a36ee2ac9b87aeeca6043e
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-05-21 12:49:40 -0300

    Update JS_ERE_BANCOS_ALL.js

--- JS_ERE_BANCOS_ALL.js
+++ JS_ERE_BANCOS_ALL.js
@@ -4,13 +4,14 @@ $( document ).ready(function() {
    $("#header-right-cart .row").before($(".entrar-ou-cadastrar"));
    $(".pagebuilder.clearfix").children().eq(0).addClass("container-carrossel");
    
-   if($(".pagebuilder.clearfix")){
+   if ($(".pagebuilder.clearfix")) {
       
       var Link_Pagina_Cadastro = $("<link />", {
             rel: "stylesheet",
             type: "text/css",
             href: "https://pedrohbar.github.io/Styles/CSS_ERE_BANCOS_INDEX.css"
-        })
+        });
+      
       $("head").append(Link_Pagina_Cadastro);
       
    }
