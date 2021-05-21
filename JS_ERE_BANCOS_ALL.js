4f82f9ff8f459a64afe529bcc1675404c59a60c1
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-05-21 12:07:23 -0300

    Update JS_ERE_BANCOS_ALL.js

--- JS_ERE_BANCOS_ALL.js
+++ JS_ERE_BANCOS_ALL.js
@@ -2,5 +2,6 @@ $( document ).ready(function() {
    $("#header-search, .caret").remove();
    $("#header_logo").after($(".texto-menu-ere-bancos"));
    $("#header-right-cart .row").before($(".entrar-ou-cadastrar"));
+   $(".pagebuilder.clearfix").children().eq(0).addClass("container-carrossel")
 
 });
