e3916bb42a9d9cbdf8a07548b94fb558be20587b
Pedrohbar <77450396+Pedrohbar@users.noreply.github.com>
2021-06-02 11:13:46 -0300

    Update JS_ERE_BANCOS_ALL.js

--- JS_ERE_BANCOS_ALL.js
+++ JS_ERE_BANCOS_ALL.js
@@ -85,7 +85,7 @@ $(document).ready(function () {
 
 
     }
-    else if($(".lists.tree.block-category.dynamized")[0]) {
+    if($(".lists.tree.block-category.dynamized")[0]) {
         var Link_Pagina_Selecao_De_Produto = $("<link />", {
             rel: "stylesheet",
             type: "text/css",
